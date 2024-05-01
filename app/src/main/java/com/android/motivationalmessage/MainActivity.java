12ab8d0f26fc19938d89080fc210dc53fad20cb4
Keke <kekemabule19990216@gmail.com>
2024-05-01 19:36:35 +0200

    Updated Gradle Files

--- app/src/main/java/com/android/motivationalmessage/MainActivity.java
+++ app/src/main/java/com/android/motivationalmessage/MainActivity.java
@@ -1,14 +1,14 @@
 package com.android.motivationalmessage;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
-
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.*;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,40 +23,33 @@ public class MainActivity extends AppCompatActivity
         etName = findViewById(R.id.etName);
         btnGenerateMsg = findViewById(R.id.btnGenerateMsg);
 
-        btnGenerateMsg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                String name = etName.getText().toString();
+        btnGenerateMsg.setOnClickListener(view -> {
+            String name = etName.getText().toString();
 
-                if (!name.isEmpty()) {
-                    String[] arrayString = {
-                            "Hi " + name + ", you are amazing!",
-                            "Hi " + name + ", just remember, nobody owes you anything.",
-                            "Hi " + name + ", just hang in there, you got this.",
-                            "Hi " + name + ", remember, giving up is never the option.",
-                            "Hi " + name + ", remember why you started in the first place.",
-                            "Hi " + name + ", you are better than you think."
-                    };
+            if (!name.isEmpty()) {
+                String[] arrayString = {
+                        "Hi " + name + ", you are amazing!",
+                        "Hi " + name + ", just remember, nobody owes you anything.",
+                        "Hi " + name + ", just hang in there, you got this.",
+                        "Hi " + name + ", remember, giving up is never the option.",
+                        "Hi " + name + ", remember why you started in the first place.",
+                        "Hi " + name + ", you are better than you think."
+                };
 
-                    Random random = new Random();
+                Random random = new Random();
 
-                    int randomString = random.nextInt(arrayString.length);
+                int randomString = random.nextInt(arrayString.length);
 
-                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                    dialog.setMessage(arrayString[randomString]);
-                    dialog.setNegativeButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            //No action needed here
-                        }
-                    });
-                    dialog.show();
-                    etName.setText("");
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Please enter your name", Toast.LENGTH_SHORT).show();
-                }
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setMessage(arrayString[randomString]);
+                dialog.setNegativeButton("OK", (dialogInterface, i) -> {
+                    //No action needed here
+                });
+                dialog.show();
+                etName.setText("");
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Please enter your name", Toast.LENGTH_SHORT).show();
             }
         });
     }
